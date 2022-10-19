6e7113fbb459cb88e34c7d08962b12087bf5d075
unknown <arturodiazmay@gmail.com>
2022-10-19 10:53:53 -0500

    Completed PostList Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import Post from './components/Post/Post';
 import PostList from './components/PostList/PostList';
 
 function App() {
   
-  const [posts, setPosts] = useState([{author: 'JJ Vega', postText: 'Its April Fools Day! Give this a dislike if you really like it. : )'}])
+  const [posts, setPosts] = useState([{author: 'JJ Vega', postText: 'Its April Fools Day! Give this a dislike if you really like it. : )'}, {author: 'Arturo', postText: 'Pineapple goes on pizza'}])
 
   return (
     <div>
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,3 +1,4 @@
+import Post from "../Post/Post";
 
 const PostList = (props) => {
     return ( 
@@ -5,7 +6,7 @@ const PostList = (props) => {
             {props.parentPosts.map((post) => 
                 //render Post components here, give it the needed props
                 //implicit return, don't need curly braces or 'return' keyword
-                
+                <Post author={post.author} postText={post.postText} />
             )}
         </div>
      );
